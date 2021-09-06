f4f399932b757c5fc53153cc7c38f2dd8606f30b
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2021-09-06 17:26:35 +0100

    Created triggers

---
+++ rovers-project/Assets/Scripts/exposedMed.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exposedMed : MonoBehaviour
+{
+    private void OnTriggerEnter()
+        {
+            Debug.Log("Radiation Detected: MEDIUM");
+
+        }
+}
