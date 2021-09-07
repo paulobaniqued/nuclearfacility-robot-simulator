8d83836dfcfeba0defbac7cac3a035999ca0f9e6
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2021-09-07 19:35:21 +0100

    Add health bars

--- rovers-project/Assets/Scripts/individualDetector.cs
+++ rovers-project/Assets/Scripts/individualDetector.cs
@@ -1,22 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class individualDetector : MonoBehaviour
 {
+    [SerializeField] private Slider healthbarSlider;
+
+    private int maximumHealth;
+    private int currentHealth;
+
 
     void Start()
     {
         gameObject.tag = "Component";
+        maximumHealth = 100;
+        currentHealth = maximumHealth;
+        SetHealthbarUI();
     }
 
-    private void OnTriggerEnter(Collider other)
+    // Make this tick on timer. Maybe use OnTriggerStay?
+    private void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Radiation")
         {
             Debug.Log("Radiation Detected on component: " + gameObject.name);
             Debug.Log("Radiation level: " + other.name);
+
+            if (other.name == "radiation-low")
+            {
+                currentHealth = currentHealth - 3;
+                SetHealthbarUI();
+            }
+            else if (other.name == "radiation-med")
+            {
+                currentHealth = currentHealth - 5;
+                SetHealthbarUI();
+            }
+            else if (other.name == "radiation-high")
+            {
+                currentHealth = currentHealth - 8;
+                SetHealthbarUI();
+            }
+
         }
+    }
+
+    private void SetHealthbarUI()
+    {
+        healthbarSlider.value = currentHealth;
+
     }
 
 
