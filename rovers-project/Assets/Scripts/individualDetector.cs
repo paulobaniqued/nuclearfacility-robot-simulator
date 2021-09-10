482ebe02f3694d6c66c8632334d3146897b1726b
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2021-09-10 16:27:36 +0100

    9-Sept Update

--- rovers-project/Assets/Scripts/individualDetector.cs
+++ rovers-project/Assets/Scripts/individualDetector.cs
@@ -7,39 +7,65 @@ public class individualDetector : MonoBehaviour
 {
     [SerializeField] private Slider healthbarSlider;
 
-    private int maximumHealth;
-    private int currentHealth;
+    public float maximumHealth;
+    public float currentHealth;
+    private bool TimerStarted;
 
 
     void Start()
     {
         gameObject.tag = "Component";
-        maximumHealth = 100;
+        maximumHealth = 100f;
         currentHealth = maximumHealth;
+        TimerStarted = false;
         SetHealthbarUI();
     }
 
-    // Make this tick on timer. Maybe use OnTriggerStay?
-    private void OnTriggerEnter(Collider other) 
+    // Initial damage to component on trigger enter
+    private void OnTriggerEnter(Collider radCollider) 
     {
-        if (other.tag == "Radiation")
+        if (radCollider.tag == "Radiation")
         {
             Debug.Log("Radiation Detected on component: " + gameObject.name);
-            Debug.Log("Radiation level: " + other.name);
+            Debug.Log("Radiation level: " + radCollider.name);
 
-            if (other.name == "radiation-low")
+            if (radCollider.name == "radiation-low")
             {
-                currentHealth = currentHealth - 3;
+                currentHealth = currentHealth - 3.0f;
                 SetHealthbarUI();
             }
-            else if (other.name == "radiation-med")
+            else if (radCollider.name == "radiation-med")
             {
-                currentHealth = currentHealth - 5;
+                currentHealth = currentHealth - 5.0f;
                 SetHealthbarUI();
             }
-            else if (other.name == "radiation-high")
+            else if (radCollider.name == "radiation-high")
             {
-                currentHealth = currentHealth - 8;
+                currentHealth = currentHealth - 8.0f;
+                SetHealthbarUI();
+            }
+
+        }
+    }
+
+    // Damage ticks per frame staying in the radiation site
+    private void OnTriggerStay(Collider radCollider) 
+    {
+        if (radCollider.tag == "Radiation")
+        {
+            if (radCollider.name == "radiation-low")
+            {
+                currentHealth = currentHealth - 0.03f;
+                SetHealthbarUI();
+            }
+            else if (radCollider.name == "radiation-med")
+            {
+                currentHealth = currentHealth - 0.05f;
+                SetHealthbarUI();
+            }
+            else if (radCollider.name == "radiation-high")
+            {
+                currentHealth = currentHealth - 0.08f;
                 SetHealthbarUI();
             }
 
