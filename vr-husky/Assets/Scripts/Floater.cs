890023c0fe2986f2d70a5ed41016f7d4764c9437
Paul Dominick Baniqued <47854500+paulobaniqued@users.noreply.github.com>
2021-11-10 11:39:26 +0000

    Integrate VR to Husky Sim

---
+++ vr-husky/Assets/Scripts/Floater.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Floater : MonoBehaviour
+{
+    public float underWaterDrag = 3f;
+    public float underWaterAngularDrag = 1f;
+    public float airDrag = 0f;
+    public float airAngularDrag = 0.05f;
+    public float floatingPower = 15f;
+    public float waterHeight = 0f;
+
+    Rigidbody m_Rigidbody;
+    bool underwater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float difference = transform.position.y - waterHeight;
+
+        if(difference < 0)
+        {
+            m_Rigidbody.AddForceAtPosition (Vector3.up * floatingPower * Mathf.Abs(difference), transform.position, ForceMode.Force);
+            if(!underwater)
+            {
+                underwater = true;
+                SwitchState(true);
+            }
+        }
+        else if(underwater)
+        {
+            underwater = false;
+            SwitchState(false);
+        }
+    }
+
+    void SwitchState(bool isUnderwater)
+    {
+        if(isUnderwater)
+        {
+            m_Rigidbody.drag = underWaterDrag;
+            m_Rigidbody.angularDrag = underWaterAngularDrag;
+        }
+        else
+        {
+            m_Rigidbody.drag = airDrag;
+            m_Rigidbody.angularDrag = airAngularDrag;
+        }
+    }
+}
---
+++ vr-husky/Assets/Scripts/individualDetector.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class individualDetector : MonoBehaviour
+{
+    [SerializeField] private Slider healthbarSlider;
+
+    public float maximumHealth;
+    public float currentHealth;
+    private bool TimerStarted;
+
+
+    void Start()
+    {
+        gameObject.tag = "Component";
+        maximumHealth = 100f;
+        currentHealth = maximumHealth;
+        TimerStarted = false;
+        SetHealthbarUI();
+    }
+
+    // Initial damage to component on trigger enter
+    private void OnTriggerEnter(Collider radCollider) 
+    {
+        if (radCollider.tag == "Radiation")
+        {
+            Debug.Log("Radiation Detected on component: " + gameObject.name);
+            Debug.Log("Radiation level: " + radCollider.name);
+
+            if (radCollider.name == "radiation-low")
+            {
+                currentHealth = currentHealth - 3.0f;
+                SetHealthbarUI();
+            }
+            else if (radCollider.name == "radiation-med")
+            {
+                currentHealth = currentHealth - 5.0f;
+                SetHealthbarUI();
+            }
+            else if (radCollider.name == "radiation-high")
+            {
+                currentHealth = currentHealth - 8.0f;
+                SetHealthbarUI();
+            }
+
+        }
+    }
+
+    // Damage ticks per frame staying in the radiation site
+    private void OnTriggerStay(Collider radCollider) 
+    {
+        if (radCollider.tag == "Radiation")
+        {
+            if (radCollider.name == "radiation-low")
+            {
+                currentHealth = currentHealth - 0.03f;
+                SetHealthbarUI();
+            }
+            else if (radCollider.name == "radiation-med")
+            {
+                currentHealth = currentHealth - 0.05f;
+                SetHealthbarUI();
+            }
+            else if (radCollider.name == "radiation-high")
+            {
+                currentHealth = currentHealth - 0.08f;
+                SetHealthbarUI();
+            }
+
+        }
+    }
+
+    private void SetHealthbarUI()
+    {
+        healthbarSlider.value = currentHealth;
+
+    }
+
+
+}
